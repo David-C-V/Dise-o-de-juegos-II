34f794e0e1bbc88d9493d01eec0ba3d65c2e902a
David-C-V <144165797+David-C-V@users.noreply.github.com>
2025-01-27 19:33:17 -0600

    Inicio

---
+++ 1.4 Inicio del proyecto/Assets/HolaMundo.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Imprimir el mensaje en la consola
+        Debug.Log("Hola mundo");
+    }
+}
